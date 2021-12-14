a105b9bff701ff0e79fd8383b54073435d4742e6
Goldy Gour <64465355+21goldy@users.noreply.github.com>
2021-12-14 10:43:15 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# day1 # project1
+print("Hey! This is a Band name generating program...")
+fav_band = str(input("Enter your favourite band name: "))
+fav_avenger = str(input("Enter your favourite avenger: "))
+lucky_num = str(input("Enter your lucky number: "))
+band_name = lucky_num + "_" + fav_avenger + "_" + fav_band
+print("Your band name can be ", band_name)
